46e497d205ac785ee4c9e00f8f4eed4e118edc68
psyll0n <45129366+psyll0n@users.noreply.github.com>
2021-02-22 16:53:08 +0200

    Initial commit.

---
+++ looping_thru_dictionaries.py
@@ -0,0 +1,18 @@
+# Define a basic dictionary
+
+person = {'name': 'Kalob',
+	'course': 'Python for Everybody',
+	'role': 'Teacher'}
+
+# Loop through all keys in in the 'person' dictionary and print each key value pair.
+# Example "unpacking of a dictionary in python
+
+for key in person:
+	value = person[key]
+	print(key, value)
+   
+
+person.items()
+
+for key, value in person.items():
+	print(f"Key: {key} \t\t Value: {value}")
