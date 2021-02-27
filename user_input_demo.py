1f29d2e966197ec51caf134aad4826ea93566353
Alex Yakimov <alex.jakimov@gmail.com>
2021-02-27 14:40:32 +0200

    Renamed name_user_inpyt.py to user_input_demo.py

---
+++ user_input_demo.py
@@ -0,0 +1,14 @@
+print('Hello world!')
+
+# Ask for the person's name
+print('What is your name?') 
+
+myName = input()
+print('It is good to meet you, ' + myName)
+print('The length of your name is:') 
+print(len(myName))
+
+# Ask for the person's age
+print('What is your age?')
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
