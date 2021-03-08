11d476971727eccd7026b07da93d15cf692f8e0f
psyll0n <45129366+psyll0n@users.noreply.github.com>
2021-03-08 23:32:18 +0200

    Adding tuple_update_workaround.py, function_example.py, recursion_function.py scripts to the repo.

--- tuple_update_workaround.py
+++ tuple_update_workaround.py
@@ -1,3 +1,4 @@
+
 # Once a tuple is created, you cannot change its values. Tuples are unchangeable, or immutable as it also is called.
 # But there is a workaround. You can convert the tuple into a list, change the list, and convert the list back into a tuple.
 
@@ -12,3 +13,28 @@ print(y)
 x = tuple(y)
 
 print(x)
+
+# Apend to an existing tuple in the following way.
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+y = list(thistuple)
+y.append("orange")
+print(y)
+
+thistuple = tuple(y)
+print(thistuple)
+
+
+# Remove items from a tuple in the following way.
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+y = list(thistuple)
+y.remove("apple")
+print(y)
+
+thistuple = tuple(y)
+print(thistuple)
