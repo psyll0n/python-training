caa7c6ebfe302f64d6a4d71db76c9cb31d9c7071
psyll0n <45129366+psyll0n@users.noreply.github.com>
2021-03-29 15:20:01 +0300

    Adding class_methods_example.py creating_class_example.py lambda_example.py to the repo.

---
+++ class_methods_example.py
@@ -0,0 +1,30 @@
+
+
+class MyClass:
+    course = "Python for Everybody."
+
+    def __init__(self, students):
+         self.students = students
+
+    def get_total_students(self):
+        print(f"The total number of students is: {self.students}")
+
+    def thing(self, param1="Thing"):
+         print(param1)
+
+    def add_students(self):
+         self.students = self.students + 1
+         print("New total is", self.students)
+
+my_class = MyClass(100_000)
+
+my_class.thing()
+my_class.thing("Something else")
+
+my_class.get_total_students()
+
+print(my_class.students)
+my_class.add_students()
+
+print(my_class.students)
+
