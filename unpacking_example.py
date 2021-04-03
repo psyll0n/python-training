3d6072d628c0ce7b85e6851527b1d2c571c26650
Alex Yakimov <alex.jakimov@gmail.com>
2021-04-03 16:24:06 +0300

    Adding unpacking_example.py to the repo.

---
+++ unpacking_example.py
@@ -0,0 +1,16 @@
+# Example unplacking of arguments.
+
+from sys import argv, version
+import warnings
+
+warnings.filterwarnings("ignore")
+
+try:
+    script, first, second, third = argv
+    print("The script is called:", script)
+    print("Your first variable is:", first)
+    print("Your third variable is:", second) 
+    print("Your fourth variable is:", third)
+except:
+    print("example: python3 unpacking_example.py first second third")
+
