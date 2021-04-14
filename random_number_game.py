d3a6b671133682027dc23382713872cc5e40c108
Alex Yakimov <alex.jakimov@gmail.com>
2021-04-14 22:15:08 +0300

    "Modified the random_number_game.py script.

--- random_number_game.py
+++ random_number_game.py
@@ -19,8 +19,11 @@ except:
 
 try:
     while guess != random:
-        guess = int(input(f"Specify a number between {minimum} and {maximum}: "))
-        guesses.append(guess)
+        try:
+            guess = int(input(f"Specify a number between {minimum} and {maximum}: "))
+            guesses.append(guess)
+        except:
+            print("You have not entered a number.")
         if guess < random:
             print("Your number is below the correct number.")
         elif guess > random: 
@@ -28,9 +31,7 @@ try:
         elif guess == random:
             print("Good job. You have guessed the correct number!")
             print(f"Your previous guesses: {guesses}")
-        else:
-            print("You have not specified a number.")
- 
+       
 except:
     print("Please, specify a number between 1 and 20.")
 
