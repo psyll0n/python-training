dff133de4116cf9fa1c56045e7a0a8344b9e3813
Alex Yakimov <alex.jakimov@gmail.com>
2021-04-28 17:35:55 +0300

    Adding generator_function.py to the repo.

---
+++ generator_function.py
@@ -0,0 +1,21 @@
+import time
+
+n = int(input("Please, enter a number to start the countdown: "))
+
+# Sample function using a generator. 
+def countdown(n):
+    print('Starting to count down from', n,'...')
+    while n > 0:
+        yield n
+        n -= 1
+
+
+# Create the generator.
+
+c = countdown(n)
+
+for i in range(0, n):
+    print(next(c))
+    time.sleep(1)
+print('Done!')
+
