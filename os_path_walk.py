66266f709bf4cb10e39d2347f05f4cc32ddf1eea
Alex Yakimov <alex.jakimov@gmail.com>
2021-05-01 13:11:22 +0300

    Adding sys.argv_example.py, sys.argv_example.py and sys_module_example.py to the repo.

--- os_path_walk.py
+++ os_path_walk.py
@@ -5,9 +5,16 @@ import os
 
 # os.listdir returns the contents of a directory.
 def walk_path(parent_path):
-    print(f"Checking: {parent_path}")
-    childs = os.listdir(parent_path)
-    
+    for parent_path, directories, files in os.walk(parent_path):
+        print(f"Checking: {parent_path}")
+        childs = os.listdir(parent_path)
+        for file_name in files:
+            file_path = os.path.join(parent_path, file_name)
+            last_access = os.path.join(file_path)
+            size = os.path.getsize(file_path)
+            print(f"File: {file_path}")
+            print(f"\tlast accessed: {last_access}")
+            print(f"\tsize: {size}")
     # Construct the full path of an item in the parent directory.
     for child in childs:
         child_path = os.path.join(parent_path, child)
---
+++ sys.argv_example.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+Simple command-line tool using sys.argv
+"""
+
+try:
+    import sys
+
+    if __name__ == '__main__':
+        print(f"The first argument: '{sys.argv[0]}'")
+        print(f"The second argument: '{sys.argv[1]}'")
+        print(f"The third argument: '{sys.argv[2]}'")
+        print(f"The fourth argument: '{sys.argv[3]}'")
+except:
+    print("example: ./sys.argv_example.py --a-flag some-value 13")
