48f3bf8c697db5b93c70f7d65c0d738f1f6cbd7a
Alex Yakimov <alex.jakimov@gmail.com>
2021-05-03 19:21:06 +0300

    Adding string scripts with string manipulation examples and also regex pattern matching.

---
+++ regex_pattern_matching.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+# Pattern matching using regex.
+
+import re
+
+
+""" While there are several steps to using regular expressions in Python, each
+step is fairly simple.
+
+1. Import the regex module with import re.
+2. Create a Regex object with the re.compile() function. (Remember to use a
+raw string.)
+3. Pass the string you want to search into the Regex object’s search() method.
+
+This returns a Match object.
+
+Call the Match object’s group() method to return a string of the actual
+matched text."""
+
+
+
+phoneNumRegex = re.compile(r'(\d{3})-?(\d{3})-(\d{4})')
+matchedObject = phoneNumRegex.search('My number is 415-555-1011.')
+print('Phone number found: ' + matchedObject.group())
+
+# Matched objext groups can be separated with '()' and printed out as shown below.
+print(matchedObject.group())
+print(matchedObject.group(1))
+print(matchedObject.group(2))
+print(matchedObject.group(3))
---
+++ sumup_integers_in_range.py
@@ -0,0 +1,11 @@
+# Given two integers a and b, which can be positive or negative, find the sum of all the
+# integers between including them too and return it. If the two numbers are equal return a or b.
+
+a = int(input("Enter a number for a: "))
+b = int(input("Enter a number for b: "))
+
+def get_sum(a,b):
+    return sum(range(min(a, b), max(a, b) + 1))
+    
+
+print(get_sum(a, b))
