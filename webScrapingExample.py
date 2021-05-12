1ae8cc57671c1b4f4c0c66180f37739478fb97cb
Alex Yakimov <alex.jakimov@gmail.com>
2021-05-12 17:34:15 +0300

    Re-formatted the code of the script that were recently uploaded.

--- webScrapingExample.py
+++ webScrapingExample.py
@@ -1,23 +1,22 @@
 #! python3
-# webScrapingExample.py - a script that uses python's 'webbrowser' built-in module for web scraping. 
+# webScrapingExample.py - a script that uses python's 'webbrowser' built-in module for web scraping.
 # Gets a street address from the command line arguments or clipboard.
 # Opens the web browser to the Google Maps page for the address.
 
-'''
+"""
 - Reads the command line arguments from sys.argv.
 - Reads the clipboard contents.
 - Calls the webbrowser.open() function to open the web browser.
-'''
+"""
 
 import webbrowser, sys, pyperclip
 
 
 if len(sys.argv) > 1:
     # Get address from the command line.
-    address = ' '.join(sys.argv[1:])
-else: 
-    # Get address from clipboard. 
+    address = " ".join(sys.argv[1:])
+else:
+    # Get address from clipboard.
     address = pyperclip.paste()
-    
-webbrowser.open('https://www.google.com/maps/place/' + address)
 
+webbrowser.open("https://www.google.com/maps/place/" + address)
