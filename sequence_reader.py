90b9d8b5616142c27c60ea687487c7f5ef255757
psyll0n <alex.jakimov@gmail.com>
2021-06-18 11:42:07 +0300

    Performed refactoring in sequence_reader.py

--- sequence_reader.py
+++ sequence_reader.py
@@ -7,20 +7,15 @@ import sys
 def read_series(filename):
     try:
         f = open(filename, mode='rt', encoding='utf-8')
-        series = []
-        for line in f:
-            a = int(line.strip())
-            series.append(a)
+        return [int(line.strip()) for line in f]
     finally:
         f.close
-    return series
-
+        
         
 def main(filename):
     series = read_series(filename)
     print(series)
     
 if __name__ == '__main__':
-    read_series()
-    
+    read_series(filename)    
     
