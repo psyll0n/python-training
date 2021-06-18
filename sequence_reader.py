91fd0ef0041472d5fb0a5c732925bf446fc24cad
psyll0n <alex.jakimov@gmail.com>
2021-06-18 11:30:38 +0300

    Modified sequence_reader.py

--- sequence_reader.py
+++ sequence_reader.py
@@ -4,14 +4,16 @@
 import sys
 
 def read_series(filename):
-    f = open(filename, mode='rt', encoding='utf-8')
-    series = []
-    for line in f:
-        a = int(line.strip())
-        series.append(a)
+    try:
+        f = open(filename, mode='rt', encoding='utf-8')
+        series = []
+        for line in f:
+            a = int(line.strip())
+            series.append(a)
+    finally:
         f.close
-        return series
-    
+
+        
 def main(filename):
     series = read_series(filename)
     print(series)
