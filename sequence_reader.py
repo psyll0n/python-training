3e569e0b34f51690bee66806361a294ff8ea3280
psyll0n <alex.jakimov@gmail.com>
2021-06-18 11:38:13 +0300

    Modified sequence_reader.py

--- sequence_reader.py
+++ sequence_reader.py
@@ -1,6 +1,7 @@
 #! python3
 # sequence_reader.py - Auxiliary python script used to read the sequence of numbers generated with recaman_sequence.py.
 
+from os import name
 import sys
 
 def read_series(filename):
@@ -18,5 +19,8 @@ def read_series(filename):
 def main(filename):
     series = read_series(filename)
     print(series)
+    
+if __name__ == '__main__':
+    read_series()
     
     
