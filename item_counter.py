2e37e72f807bfc7e72594f3d1d9564b732d964db
psyll0n <alex.jakimov@gmail.com>
2021-07-15 17:09:50 +0300

    Adding examples of filtering, sorting and item counting algorithms

---
+++ item_counter.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# item_counter.py - Using a hash table to count individual items.
+
+
+# Define a set of items in which we want to count.
+items = [
+    "apple",
+    "orange",
+    "apple",
+    "pear",
+    "orange",
+    "banana",
+    "orange",
+    "apple",
+    "pear",
+    "banana",
+    "mango",
+    "kiwi",
+    "plum",
+    "banana",
+    "pineapple",
+    "strawberry",
+    "cherry",
+    "apple",
+    "orange",
+]
+
+# TODO: Create a hash table to perform the filter operation.
+counter = dict()
+
+# TODO: Iterate over each item and increment the count for each one.
+for item in items:
+    if item in counter.keys():
+        counter[item] += 1
+    else:
+        counter[item] = 1
+
+
+# TODO: Create a set from the resulting keys in the hash table.
+print(counter)
