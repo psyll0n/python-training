58850e68e9b25706193ead1a1ae777a309c2ae02
psyll0n <alex.jakimov@gmail.com>
2021-07-28 11:44:06 +0300

    Adding python_decimal_objects

---
+++ python3_decimal_objects.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# python_decimal_objects.py - Decimal Objects are used for precision calculations.
+
+
+from decimal import Decimal
+
+
+amount = 112.31
+
+print(f'{amount:.20f}')
+
+
+principal = Decimal('1000.00')
+print(type(principal))
+
+rate = Decimal('0.05')
+print(type(rate))
+
+x = Decimal('10.5')
+y = Decimal('2')
+
+print(x // y)
+print(x + y)
+
+
+for year in range(1, 11):
+    amount = principal * (1 + rate) ** year
+    print(f'{year:>2}{amount:>10.2f}')
