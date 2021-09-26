b16c7096cb735ec970b1842b825b0b28ec4a6c2f
psyll0n <45129366+psyll0n@users.noreply.github.com>
2021-09-26 17:45:31 +0300

    Adding decorator func examples and use cases.

---
+++ decorator_func_blueprint.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# Decorator function blueprint.
+
+from functools import wraps
+
+# Note: @wraps takes a function to be decorated and adds the functionality of copying
+# over the function name, docstring, arguments list, etc. This allows us to access the
+# pre-decorated function’s properties in the decorator.
+
+def decorator_name(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if not can_run:
+            return "Function will not run"
+        return f(*args, **kwargs)
+    return decorated
+
+
+@decorator_name
+def func():
+    """Function Docstring"""
+    print("Function is running.")
+    
+can_run = True
+print(func())
+
+
+can_run = False
+print (func())
+
