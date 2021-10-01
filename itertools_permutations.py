d8a4c87bff7f0bfafc54ede197216818c771179a
psyll0n <alex.jakimov@gmail.com>
2021-10-01 13:49:59 +0300

    Applied directory subdivision for common script categories

--- itertools_permutations.py
+++
@@ -1,9 +0,0 @@
-#! python3
-# itertools: product, permutations, combinations, accumulate, groupby, and infinite iterators
-
-from itertools import permutations
-
-
-a = [1, 2, 3]
-perm = permutations(a)
-print(list(perm))
--- while_loop_example2.py
+++
@@ -1,9 +0,0 @@
-# Ask for user input first.
-
-num = int(input("Please, enter a number betwee 1 and 99: "))
-
-# Use the user input to loop through the numnbers until it reaches to 100.
-
-while num <= 100:
-    print(num)
-    num = num + 1
