c91a8402cedf1c9fdfdba6fa80464ad073234cbd
psyll0n <45129366+psyll0n@users.noreply.github.com>
2022-11-19 18:51:14 +0200

    Added a couple of python functions examples

---
+++ hypervolume.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# A simple function that calculates the area / volume of triangles, cuboids, hypercuboids
+
+def hypervolume(length, *lengths):
+    v = length
+    for item in lengths:
+        v *= item
+    return v
+
+# calculate the area of a rectangle
+hypervolume(2, 4)
+
+# calculate the volume of a cuboid
+hypervolume(2, 4, 6)
+
+# calculate the volume of a hypercuboid
+hypervolume(2, 4, 6, 8)
