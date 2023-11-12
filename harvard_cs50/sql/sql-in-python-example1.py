2aa9f639474505c7e77bd365ff0ceccdc5b7f073
psyll0n <45129366+psyll0n@users.noreply.github.com>
2023-11-12 17:47:19 +0200

    Added Python - SQL usage examples

---
+++ harvard_cs50/sql/sql-in-python-example1.py
@@ -0,0 +1,14 @@
+from cs50 import SQL
+
+db = SQL("sqlite:///favorites.db")
+
+favorite = input("Favorite: ")
+
+# `db.execute` returns a list dictionaries...
+rows = db.execute("SELECT COUNT(*) AS n FROM favorites WHERE problem = 'Mario'")
+
+row = rows[0]
+print(row["n"])
+    
+    
+    
