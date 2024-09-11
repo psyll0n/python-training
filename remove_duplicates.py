d948b012946048a7c19e5eefd78d7f00bb7598f3
Alex Yakimov <alex.jakimov@gmail.com>
2024-09-11 15:18:55 +0300

    Repo re-organization

--- remove_duplicates.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# remove_duplicates.py - Remove duplicates in a file and sorts the output.
-
-text = "to be or not to be that is the question"
-
-unique_words = set(text.split())
-
-for word in sorted(unique_words):
-    print(word, end=" ")
