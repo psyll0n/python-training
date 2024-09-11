32688b0b1ae4adb5458932cef3599884466f0df0
Alex Yakimov <alex.jakimov@gmail.com>
2024-09-11 10:32:36 +0300

    File rename

---
+++ my_collections.py
@@ -0,0 +1,11 @@
+#! python3
+# collections: Counter, namedtuple, OrderedDictionary, defaultdict, dequeue
+
+from collections import Counter
+
+a = "aaaabbbbbbbbccccccc"
+my_counter = Counter(a)
+print(my_counter.values())
+print(my_counter.most_common(1)[0][0])
+
+print(list(my_counter.elements()))
