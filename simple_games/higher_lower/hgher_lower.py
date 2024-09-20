a8d5ea4e8791ca8a8590b28f2d629f548cf6a473
psyll0n <45129366+psyll0n@users.noreply.github.com>
2024-09-20 18:34:11 +0300

    Added Higher - Lower game

---
+++ simple_games/higher_lower/hgher_lower.py
@@ -0,0 +1,82 @@
+#! python3
+
+import random
+import instagram_data
+import ascii_art
+
+# Print game logo
+print(ascii_art.game_logo)
+
+# Initialize score and game over state
+current_score = 0
+game_over = False
+
+
+def compare(instagram_followers_A, instagram_followers_B, user_input):
+    """
+    Compares the Instagram follower counts of two celebrities and updates the score.
+
+    Args:
+    instagram_followers_A (int): Follower count of celebrity A.
+    instagram_followers_B (int): Follower count of celebrity B.
+    user_input (str): The user's choice, either "A" or "B".
+
+    Returns:
+    None: Updates global variables for game state and score.
+    """
+    global game_over
+    global current_score
+
+    if user_input == "A" and instagram_followers_A > instagram_followers_B:
+        current_score += 1
+        print(f"You are right! Current score: {current_score}")
+    elif user_input == "B" and instagram_followers_B > instagram_followers_A:
+        current_score += 1
+        print(f"You are right! Current score: {current_score}")
+    else:
+        game_over = True
+        print(ascii_art.game_logo)
+        print(f"Sorry, that's wrong! Final score: {current_score}")
+
+
+# Main game loop
+while not game_over:
+
+    # Randomly choose two celebrities from the data
+    celebrity_A = random.choice(instagram_data.data)
+    celebrity_B = random.choice(instagram_data.data)
+
+    # Ensure both chosen celebrities are not the same
+    while celebrity_A == celebrity_B:
+        celebrity_B = random.choice(instagram_data.data)
+
+    # Access the relevant fields for A
+    celebrity_name_A = celebrity_A["name"]
+    description_celebrity_A = celebrity_A["description"]
+    instagram_followers_A = celebrity_A["follower_count"]
+    country_celebrity_A = celebrity_A["country"]
+
+    # Access the relevant fields for B
+    celebrity_name_B = celebrity_B["name"]
+    description_celebrity_B = celebrity_B["description"]
+    instagram_followers_B = celebrity_B["follower_count"]
+    country_celebrity_B = celebrity_B["country"]
+
+    # Print details for celebrity A
+    print(f"Compare A: {celebrity_name_A}, {
+          description_celebrity_A}, from {country_celebrity_A}")
+
+    # Print the Vs. ASCII art
+    print(ascii_art.vs_logo)
+
+    # Print details for celebrity B
+    print(f"Against B: {celebrity_name_B}, {
+          description_celebrity_B}, from {country_celebrity_B}")
+
+    # Prompt user to choose who has more Instagram followers
+    user_input = input(
+        "Who has more Instagram followers? Type 'A' or 'B': ").strip().upper()
+    print("\n" * 50)
+
+    # Compare the follower counts and check if the user is correct
+    compare(instagram_followers_A, instagram_followers_B, user_input)
---
+++ simple_games/higher_lower/instagram_data.py
@@ -0,0 +1,113 @@
+data = [
+    {
+        "name": "Christiano Ronaldo",
+        "follower_count": 639656406,
+        "description": "Footballer",
+        "country": "Portugal"
+
+    },
+    {
+        "name": "Leo Messi",
+        "follower_count": 504160360,
+        "description": "Footballer",
+        "country": "Argentina",
+
+    },
+    {
+        "name": "Selena Gomez",
+        "follower_count": 424438920,
+        "description": "Musician and actress",
+        "country": "United States"
+
+    },
+    {
+        "name": "Dwayne Johnson",
+        "follower_count": 395357315,
+        "description": "Actor and professional wrestler",
+        "country": "United States"
+
+    },
+    {
+        "name": "Kylie Jenner",
+        "follower_count": 396874874,
+        "description": "Reality TV personality and business woman. Self-made billionaire.",
+        "country": "United States"
+
+    },
+    {
+        "name": "Ariana Grande",
+        "follower_count": 376894813,
+        "description": "Musician and actress",
+        "country": "United States"
+
+    },
+    {
+        "name": "Kim Kardashian",
+        "follower_count": 360677218,
+        "description": "Reality TV personality and business woman.",
+        "country": "United States",
+
+    },
+
+    {
+        "name": "Beyonce",
+        "follower_count": 316317841,
+        "description": "Musician and actress",
+        "country": "United States",
+
+    },
+    {
+        "name": "Khloe Kardashian",
+        "follower_count": 306871998,
+        "description": "Reality TV personality and business woman.",
+        "country": "United States",
+    },
+    {
+        "name": "Justin Bieber",
+        "follower_count": 294418982,
+        "description": "Musician",
+        "country": "United States",
+    },
+    {
+        "name": "Kendall Jenner",
+        "follower_count": 291429019,
+        "description": "Reality TV personality and business woman.",
+        "country": "United States",
+    },
+    {
+        "name": "Taylor Swift",
+        "follower_count": 284120377,
+        "description": "Musician",
+        "country": "United States",
+    },
+    {
+        "name": "Virat Kohli",
+        "follower_count": 270792113,
+        "description": "Cricketer",
+        "country": "India",
+    },
+    {
+        "name": "Jennifer Lopez",
+        "follower_count": 251340768,
+        "description": "Musician and actress",
+        "country": "United States",
+    },
+    {
+        "name": "Nicki Minaj",
+        "follower_count": 227990090,
+        "description": "Musician",
+        "country": "United States",
+    },
+    {
+        "name": "Kourtney Kardashian",
+        "follower_count": 224776722,
+        "description": "American media personality and socialite.",
+        "country": "United States",
+    },
+    {
+        "name": "Neymar da Silva Santos Jr",
+        "follower_count": 221750492,
+        "description": "Footballer",
+        "country": "Brazil",
+    },
+]
