dfd5539e63193063717099456be4129830268d80
psyll0n <45129366+psyll0n@users.noreply.github.com>
2024-09-30 18:09:17 +0300

    Added Turtle graphics module examples

--- OOP/turtle_graphics/spirograph.py
+++ OOP/turtle_graphics/spirograph.py
@@ -7,17 +7,20 @@ turtle.colormode(255) # Sets the color mode to RGB
 t.shape("turtle")
 
 
-for _ in range(100):
-    r = 100
-    t.speed("fastest")
-    t.circle(r)
-    r = random.randrange(0, 256)
-    g = random.randrange(0, 256)
-    b = random.randrange(0, 256)
-    t.color(r, g, b)
-    current_heading = t.heading()
-    t.setheading(current_heading + 5)
+def draw_spirograph(size_of_gap):
 
+    for _ in range(int(360 / size_of_gap)):
+        r = 100
+        t.speed("fast")
+        t.circle(r)
+        r = random.randrange(0, 256)
+        g = random.randrange(0, 256)
+        b = random.randrange(0, 256)
+        t.color(r, g, b)
+        t.setheading(t.heading() + size_of_gap)
+
+
+draw_spirograph(5)
 
 screen = turtle.Screen()
 screen.exitonclick()
