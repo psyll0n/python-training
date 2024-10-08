4a8a14ddec3b863ea9e1c5f2747d612f1d33176f
psyll0n <45129366+psyll0n@users.noreply.github.com>
2024-10-08 18:45:49 +0300

    Modified file manipulation example code

--- misc/mail_merge/main.py
+++ misc/mail_merge/main.py
@@ -1,7 +1,7 @@
 # Create a letter using starting_letter.txt
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
+# for each name in invited_names.txt
+# Replace the [name] placeholder with the actual name.
+# Save the letters in the folder "ReadyToSend".
 
 
 PLACEHOLDER = "[name]"
