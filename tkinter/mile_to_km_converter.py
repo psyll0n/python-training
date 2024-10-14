dd49d9971dd694604eb868109d3246022ec622d3
psyll0n <45129366+psyll0n@users.noreply.github.com>
2024-10-14 17:58:20 +0300

    Added Tkinter examples

--- tkinter/mile_to_km_converter.py
+++ tkinter/mile_to_km_converter.py
@@ -42,7 +42,6 @@ label_2.grid(column=1, row=1)
 label_3 = Label(window, text="Km", font=("Arial", 12, "bold"))
 label_3.grid(column=3, row=1)
 
-
 # Button widget to trigger an event when clicked.
 button = Button(window, text="Calculate", command=button_click)
 button.grid(column=1, row=2)  # Place button at the next row and column (1, 1).
